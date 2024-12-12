214366a3ba8e75ed3fe38c558dec5893afd1bfe5
Wilson Desimini <wdesimini8@gmail.com>
2024-12-12 15:31:35 -0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Wilson Desimini on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
